0159e55a075c7507e9ac452c75b4859ef1213b3c
devGabyAlves <gabriellysthefany.alves@gmail.com>
2025-11-08 15:54:40 -0300

    fix(security): add error handling to server startup

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/devGabyAlves/academy-appsec/backend/api"
 )
 
 func main() {
 	r := api.SetupRouter()
-	r.Run(":8080") 
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("failed to start server")
+	}
 }
